c8df698ef6f9199b6c1b955550da3a70d333adbd
Sven2219 <sven.suk5@gmail.com>
2020-12-21 18:51:19 +0100

    Changed icon and name

--- src/components/visualization/snapshots/quickSort/manual/ManualQSSnapshots.tsx
+++ src/components/visualization/snapshots/quickSort/manual/ManualQSSnapshots.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { IQuickSnapshots } from '../../../../helpers/interfaces';
-
 import StepButton from '../../general/StepButton';
 import Snapshot from '../Snapshot';
 
